1079b6d8a2472391a219b7ed78401cc99183bdc7
timofei00 <user@MacBook-Pro-user.local>
2025-01-19 20:35:49 +0100

    changed light theme pallete so it has normal and nice colors

--- lib/themes/lightTheme.dart
+++ lib/themes/lightTheme.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
   colorScheme: ColorScheme.light(
-    surface: const Color.fromARGB(255, 245, 245, 245),
-    primary: const Color.fromARGB(255, 200, 200, 200),
-    secondary: const Color.fromARGB(255, 230, 230, 230),
-    tertiary: const Color.fromARGB(255, 215, 215, 215),
-    inversePrimary: Colors.grey.shade800,
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade100,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade700,
   ),
 );
